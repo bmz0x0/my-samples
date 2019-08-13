6f4fc19a1c01df4468388fe41cea12bac9747f56
Toru Ito <bmz0x0@gmail.com>
2019-08-13 18:45:28 +0000

    react-todo: ボタンカウント機能の追加

--- react-todo/src/app.js
+++ react-todo/src/app.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+    this.onClickButton = this.onClickButton.bind(this);
+  }
   onClickButton() {
-    alert("alert!");
+    let { count } = this.state;
+    this.setState({count: count + 1});
   }
   render() {
-    return <button onClick={this.onClickButton}>Press</button>;
+    return (
+      <div>
+        <h1>(クリックした回数: {this.state.count})</h1>
+        <button onClick={this.onClickButton}>Press</button>
+      </div>
+    )
   }
 }
 
