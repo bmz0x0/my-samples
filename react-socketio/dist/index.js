2133af5cbf4f9ad98aea5128af8c1f33d3ad93f9
Toru Ito <bmz0x0@gmail.com>
2019-08-17 12:32:46 +0000

    react-socketio: サーバーからの定期送信機能を追加

--- react-socketio/dist/index.js
+++ react-socketio/dist/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cron = require('node-cron');
 
 app.use(express.static('./dist'));
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected.');
+  //socket.emit('chat message', 'debug');
+  socket.on('disconnect', () => {
+    console.log('a user disconnected.');
+  });
+});
+
+cron.schedule('*/3 * * * * *', () => {
+  console.log('excute cron task.');
+  io.emit('chat message', 'cron');
 });
