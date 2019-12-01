357c68456fbd9640b8b15c2e4d3381bdd737e093
Toru Ito <bmz0x0@gmail.com>
2019-12-01 07:48:21 +0000

    socketio-simple: initial commit.

---
+++ socketio-simple/index.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cron = require('node-cron');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+cron.schedule('*/5 * * * * *', () => {
+  console.log('send hello.');
+  io.emit('message', 'hello');
+});
+
